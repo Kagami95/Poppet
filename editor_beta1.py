06615ff36b643eb270f45c7e0919f816c6b07196
Echo <kagami95@protonmail.ch>
2016-08-18 01:43:19 -0400

    Added tons of stuff (needs cleanup)

--- editor_beta1.py
+++ editor_beta1.py
@@ -2,6 +2,7 @@
 
 import curses, os, sys, traceback
 from curses import panel as panels
+from curses import textpad
 
 # GLOBALS
 screen = None
@@ -14,6 +15,14 @@ ascii_window = None
 
 x = 0
 y = 0
+hex_window_originX = 0
+hex_window_originY = 0
+ascii_window_originX = 0
+ascii_window_originY = 0
+editor_top = 0
+editor_bottom = 0
+
+mode = 'hexedit' # or 'asciiedit'
 
 def setup_colors():
 	global offset_window, hex_window, ascii_window
@@ -28,7 +37,10 @@ def setup_colors():
 
 
 def setup_screen():
-	global screen, offset_window, hex_window, ascii_window, offset_panel, hex_panel, ascii_panel
+	global screen, editor_top, editor_bottom, offset_window, offset_panel
+	global hex_width, hex_panel, hex_window, hex_window_originX, hex_window_originY
+	global ascii_width, ascii_panel, ascii_window, ascii_window_originX, ascii_window_originY
+
 	screen = curses.initscr()
 	curses.cbreak()
 	curses.noecho()
@@ -37,13 +49,22 @@ def setup_screen():
 	max_height = screen.getmaxyx()[0]
 	max_width = screen.getmaxyx()[1]
 
-	offset_width = 20
-	hex_width = 4*((max_width - offset_width)/5)
-	ascii_width = ((max_width - offset_width)/5)
+	offset_width = 14
+	new_width = max_width - 8
+	ascii_width = int(round((new_width - offset_width)/4))
+	# hex_width = int(round(3*((max_width - offset_width)/4.), 0))
+	hex_width = ascii_width * 3
 
-	offset_window = curses.newwin(max_height-5, offset_width, 3, 0) # from y3 down to max-3, 12 columns from x0
-	hex_window = curses.newwin(max_height-5, hex_width, 3, offset_width) # from y3 down to max-3, 4/5 X starting at x12
-	ascii_window = curses.newwin(max_height-5, ascii_width, 3, hex_width+offset_width) # from y3 down to max-3, 1/5 X starting at 4/5 X
+	editor_top = 1
+	editor_bottom = max_height-5
+	hex_window_originX = offset_width+2
+	hex_window_originY = editor_top + 1
+	ascii_window_originX = hex_width+offset_width+4+2
+	ascii_window_originY = editor_top + 1
+
+	offset_window = curses.newwin(editor_bottom, offset_width, editor_top, 0) # from y3 down to max-3, 12 columns from x0
+	hex_window = curses.newwin(editor_bottom, hex_width+4, editor_top, offset_width) # from y3 down to max-3, 4/5 X starting at x12
+	ascii_window = curses.newwin(editor_bottom, ascii_width+4, editor_top, hex_width+offset_width+4) # from y3 down to max-3, 1/5 X starting at 4/5 X
 
 	offset_panel = panels.new_panel(offset_window)
 	hex_panel = panels.new_panel(hex_window)
@@ -57,44 +78,91 @@ def close_screen():
 	curses.endwin()
 
 def user_input():
-	global screen
+	global screen, hex_window, editor_top, editor_bottom, x, y, mode
+	buf = get_hex()
 	key = screen.getch()
-	if key == 261 and x < screen.getmaxyx()[1]-1:
-		x += 1
-		xcol = 1
-	elif key == 260 and x > 0:
-		x -= 1
-		xcol = 2
-	elif key == 259 and y > 2:
+	hex_window.addstr(30, 5, 'y: %d, x: %d, lenbufY: %d, lenbufX: %d' % (y, x, len(buf), len(buf[0])))
+	if key == 27:
+		sys.exit(0)
+	elif key == 9:
+		if mode == 'hexedit':
+			mode = 'asciiedit'
+			x = x/3
+		else:
+			mode = 'hexedit'
+			x = x*3
+	elif key == 261:
+		# if (mode == 'hexedit' and x < hex_width-2) or (mode == 'asciiedit' and x < ascii_width-1):
+		if (mode == 'hexedit' and x < len(buf[y])*3-2) or (mode == 'asciiedit' and x < len(buf[y])-1):
+			if mode == 'hexedit' and (x+2)%3 == 0:
+				x += 2
+			else:
+				x += 1
+	elif key == 260:
+		# if (mode == 'hexedit' and x > hex_window_originX) or (mode == 'asciiedit' and x > ascii_window_originX):
+		if x > 0:
+			if mode == 'hexedit' and (x)%3 == 0:
+				x -= 2
+			else:
+				x -= 1
+	elif key == 259 and y > 0:#editor_top:
 		y -= 1
-		ycol = 2
-	elif key == 258 and y < screen.getmaxyx()[0]-3:
-		y += 1
-		ycol = 1
+	elif key == 258 and y < len(buf)-1:
+		if (mode == 'hexedit' and x < len(buf[y+1]*3)-1) or (mode == 'asciiedit' and x < len(buf[y+1])):# editor_bottom-3:
+			y += 1
+	update_cursor()
 
 def update_editor():
-	global screen
-	buf(open_files[0], 0, 1000)
-	screen.addstr(str(format_hex()))
-	user_input()
+	global offset_window, hex_window, ascii_window
+	offset = 0
+	for number in range(len(get_hex())):
+		line = get_hex()[number]
+		if number == 0:
+			hex_window.addstr(25, 5, ''.join(line).decode('hex').replace('\n', '\\n'))
+		hex_text = ''
+		ascii_text = ''
+		for byte in line:
+			hex_text += byte + ' '
+			if int(byte, 16) > 0x19 and int(byte, 16) < 0x7e:
+				ascii_text += byte.decode('hex')
+			else:
+				ascii_text += '.' # Not displayable
+		offset_window.addstr(number+1, 2, '0x' + hex(offset)[2:].zfill(8))
+		hex_window.addstr(number+1, 2, hex_text)
+		ascii_window.addstr(number+1, 2, ascii_text)
+		offset += len(line)
+
+def update_cursor():
+	global screen, hex_window_originX, hex_window_originY, ascii_window_originX, ascii_window_originY, x, y, mode
+	screen.addstr(0, 0, 'y: %d, x: %d' % (y, x))
+	if mode == 'hexedit':
+		screen.move(hex_window_originY+y, hex_window_originX+x)
+	elif mode == 'asciiedit':
+		screen.move(ascii_window_originY+y, ascii_window_originX+x)
 
 def main():
-	global windows, offset_window, hex_window, ascii_window
+	global windows, offset_window, hex_window, ascii_window, disp_buffer, x ,y
 	try:
 		setup_screen()
 		setup_colors()
+
+		buf('lorem_ipsum.txt', 0, 1000)
+		update_editor()
+
 		windows = [offset_window, hex_window, ascii_window]
 		for window in windows:
-			window.addstr(0, 0, 'xxxxxxxxxxxxxxxxxx')
 			window.box()
 			window.refresh()
 
 		panels.update_panels()
 		curses.doupdate()
-		screen.getch()
 
-		open_file('lorem_ipsum.txt')
-		# update_editor()
+		screen.addstr(0, 0, 'y: %d, x: %d, buf height: %d, buf width: %d' % (y, x, len(get_lines()), len(get_lines()[0])))
+		screen.move(hex_window_originY, hex_window_originX)
+		while True:
+			user_input()
+
+		update_editor()
 	except:
 		close_screen()
 		print traceback.format_exc()
@@ -103,4 +171,3 @@ with open('functions.py', 'r') as f:
 	exec(f.read())
 main()
 close_screen()
-close_files()
--- functions.py
+++ functions.py
@@ -9,15 +9,9 @@ ERROR_READ = 3
 ERROR_WRITE = 4
 
 # GLOBALS
-open_files = []
 disp_buffer = '' # What gets shown
-width = 24-1 # 24 bytes per line
-
-def open_file(path):
-	global open_files
-	if not os.path.exists(path):
-		return ERROR_NOSUCHFILE
-	open_files.append(open(path, 'rw'))
+hex_width = 0 # 24 bytes per line
+ascii_width = 0 # 24 bytes per line
 
 def read_bytes(file, start, end):
 	try:
@@ -27,14 +21,14 @@ def read_bytes(file, start, end):
 		return ERROR_READ
 
 def get_lines(): # returns lines of length `width`
-	global disp_buffer, width
+	global disp_buffer, ascii_width
 	line = ''
 	lines = []
 	column = 0
 	for byte in disp_buffer:
 		line += byte
 		column += 1
-		if column == width:
+		if column == ascii_width:
 			lines.append(line)
 			line = ''
 			column = 0
@@ -42,33 +36,42 @@ def get_lines(): # returns lines of length `width`
 		lines.append(line)
 	return lines
 
-def format_hex():
-	global width
-	value = ''
-	offset = 0
-	addr_prefix = '0x%s | '
-	lines = get_lines()
-	for line in lines:
-		ascii_line = ''
-		hex_line = ''
-		for byte in line:
-			if int(byte.encode('hex'), 16) > 0x19 and int(byte.encode('hex'), 16) < 0x7e:
-				ascii_line += byte
-			else:
-				ascii_line += '.' # Not displayable
-			hex_line += '%s ' % byte.encode('hex')
-		if len(hex_line) < width*3:
-			hex_line += ' '*(width*3-len(hex_line)) # Fix hex_line width
-		if len(ascii_line) < width:
-			ascii_line += ' '*(width-len(ascii_line)) # Fix ascii_line width
-		value += addr_prefix % hex(offset)[2:].zfill(8) + hex_line + ' | ' + ascii_line + ' |\n'
-		offset += len(ascii_line)
+def get_hex():
+	value = []
+	for line in get_lines():
+		hex_line = []
+		for char in line:
+			hex_line.append(char.encode('hex'))
+		value.append(hex_line)
 	return value
 
-def close_files():
-	for file in open_files:
-		file.close()
+# def format_hex():
+# 	global width
+# 	value = ''
+# 	offset = 0
+# 	addr_prefix = '0x%s | '
+# 	lines = get_lines()
+# 	for line in lines:
+# 		ascii_line = ''
+# 		hex_line = ''
+# 		for byte in line:
+# 			if int(byte.encode('hex'), 16) > 0x19 and int(byte.encode('hex'), 16) < 0x7e:
+# 				ascii_line += byte
+# 			else:
+# 				ascii_line += '.' # Not displayable
+# 			hex_line += '%s ' % byte.encode('hex')
+# 		if len(hex_line) < width*3:
+# 			hex_line += ' '*(width*3-len(hex_line)) # Fix hex_line width
+# 		if len(ascii_line) < width:
+# 			ascii_line += ' '*(width-len(ascii_line)) # Fix ascii_line width
+# 		value += addr_prefix % hex(offset)[2:].zfill(8) + hex_line + ' | ' + ascii_line + ' |\n'
+# 		offset += len(ascii_line)
+# 	return value
 
 def buf(file, start, end):
 	global disp_buffer
-	disp_buffer = read_bytes(file, start, end) # start at offset 512, runs for 1536 bytes
+	if not os.path.exists(file):
+		return ERROR_NOSUCHFILE
+	with open(file, 'rw') as f:
+		f.seek(start)
+		disp_buffer = f.read(end-start)
